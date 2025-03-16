6f918e80fce230f88659fdc0b018035140ca03dc
hanixendit <hani@xendit.co>
2025-03-16 23:22:39 +0800

    test: add more statuses

--- AppointmentScheduling/AppointmentScheduler.Tests/AppointmentRepositoryTests.cs
+++ AppointmentScheduling/AppointmentScheduler.Tests/AppointmentRepositoryTests.cs
@@ -18,21 +18,53 @@ namespace AppointmentScheduler.Tests
 		}
 
 		[Theory]
-		[InlineData(1, "2025-03-16T09:15:00", "2025-03-16T09:45:00", true)]  // Overlapping
-		[InlineData(1, "2025-03-16T09:00:01", "2025-03-16T09:00:02", true)]  // Contained inside
-		[InlineData(1, "2025-03-16T08:45:00", "2025-03-16T09:00:01", true)]  // Overlapping in front
-		[InlineData(1, "2025-03-16T10:00:00", "2025-03-16T10:30:00", false)] // No overlap
-		[InlineData(1, "2025-03-16T09:00:00", "2025-03-16T09:30:00", true)]  // Exact overlap
-		[InlineData(1, "2025-03-16T08:30:00", "2025-03-16T09:00:00", false)] // Ends exactly when another starts
-		[InlineData(1, "2025-03-16T09:30:00", "2025-03-16T10:00:00", false)] // Starts exactly when another ends
-		[InlineData(2, "2025-03-16T09:15:00", "2025-03-16T09:45:00", false)] // Different doctor, overlapping
-		[InlineData(2, "2025-03-16T08:45:00", "2025-03-16T09:00:01", false)] // Different doctor, overlapping in front
-		[InlineData(2, "2025-03-16T10:00:00", "2025-03-16T10:30:00", false)] // Different doctor, no overlap
-		[InlineData(2, "2025-03-16T09:00:00", "2025-03-16T09:30:00", false)] // Different doctor, exact overlap
-		[InlineData(2, "2025-03-16T08:30:00", "2025-03-16T09:00:00", false)] // Different doctor, ends exactly when another starts
-		[InlineData(2, "2025-03-16T09:30:00", "2025-03-16T10:00:00", false)] // Different doctor, starts exactly when another ends
+		[InlineData(1, "2025-03-16T09:15:00", "2025-03-16T09:45:00", AppointmentStatus.Scheduled, true)]  // Overlapping
+		[InlineData(1, "2025-03-16T09:00:01", "2025-03-16T09:00:02", AppointmentStatus.Scheduled, true)]  // Contained inside
+		[InlineData(1, "2025-03-16T08:45:00", "2025-03-16T09:00:01", AppointmentStatus.Scheduled, true)]  // Overlapping in front
+		[InlineData(1, "2025-03-16T10:00:00", "2025-03-16T10:30:00", AppointmentStatus.Scheduled, false)] // No overlap
+		[InlineData(1, "2025-03-16T09:00:00", "2025-03-16T09:30:00", AppointmentStatus.Scheduled, true)]  // Exact overlap
+		[InlineData(1, "2025-03-16T08:30:00", "2025-03-16T09:00:00", AppointmentStatus.Scheduled, false)] // Ends exactly when another starts
+		[InlineData(1, "2025-03-16T09:30:00", "2025-03-16T10:00:00", AppointmentStatus.Scheduled, false)] // Starts exactly when another ends
+		
+		// different doctor should have no conflicts
+		[InlineData(2, "2025-03-16T09:15:00", "2025-03-16T09:45:00", AppointmentStatus.Scheduled, false)] // Different doctor, overlapping
+		[InlineData(2, "2025-03-16T08:45:00", "2025-03-16T09:00:01", AppointmentStatus.Scheduled, false)] // Different doctor, overlapping in front
+		[InlineData(2, "2025-03-16T10:00:00", "2025-03-16T10:30:00", AppointmentStatus.Scheduled, false)] // Different doctor, no overlap
+		[InlineData(2, "2025-03-16T09:00:00", "2025-03-16T09:30:00", AppointmentStatus.Scheduled, false)] // Different doctor, exact overlap
+		[InlineData(2, "2025-03-16T08:30:00", "2025-03-16T09:00:00", AppointmentStatus.Scheduled, false)] // Different doctor, ends exactly when another starts
+		[InlineData(2, "2025-03-16T09:30:00", "2025-03-16T10:00:00", AppointmentStatus.Scheduled, false)] // Different doctor, starts exactly when another ends
+
+		// cancelled status should return no conflict whether there's overlap or none.
+		[InlineData(1, "2025-03-16T09:15:00", "2025-03-16T09:45:00", AppointmentStatus.Cancelled, false)] // Overlapping
+		[InlineData(1, "2025-03-16T08:45:00", "2025-03-16T09:00:01", AppointmentStatus.Cancelled, false)] // Overlapping in front
+		[InlineData(1, "2025-03-16T10:00:00", "2025-03-16T10:30:00", AppointmentStatus.Cancelled, false)] // No overlap
+		[InlineData(1, "2025-03-16T09:00:00", "2025-03-16T09:30:00", AppointmentStatus.Cancelled, false)] // Exact overlap
+		[InlineData(1, "2025-03-16T08:30:00", "2025-03-16T09:00:00", AppointmentStatus.Cancelled, false)] // Ends exactly when another starts
+		[InlineData(1, "2025-03-16T09:30:00", "2025-03-16T10:00:00", AppointmentStatus.Cancelled, false)] // Starts exactly when another ends
+		[InlineData(2, "2025-03-16T09:15:00", "2025-03-16T09:45:00", AppointmentStatus.Cancelled, false)] // Different doctor, overlapping
+		[InlineData(2, "2025-03-16T08:45:00", "2025-03-16T09:00:01", AppointmentStatus.Cancelled, false)] // Different doctor, overlapping in front
+		[InlineData(2, "2025-03-16T10:00:00", "2025-03-16T10:30:00", AppointmentStatus.Cancelled, false)] // Different doctor, no overlap
+		[InlineData(2, "2025-03-16T09:00:00", "2025-03-16T09:30:00", AppointmentStatus.Cancelled, false)] // Different doctor, exact overlap
+		[InlineData(2, "2025-03-16T08:30:00", "2025-03-16T09:00:00", AppointmentStatus.Cancelled, false)] // Different doctor, ends exactly when another starts
+		[InlineData(2, "2025-03-16T09:30:00", "2025-03-16T10:00:00", AppointmentStatus.Cancelled, false)] // Different doctor, starts exactly when another ends
+
+		// not sure what to do with completed status, do we have guarantees that appointments will be marked as completed only when the appointment end time has lapsed?
+		// let's just treat completed as scheduled in deciding conflicts.
+		[InlineData(1, "2025-03-16T09:15:00", "2025-03-16T09:45:00", AppointmentStatus.Completed, true)]  // Overlapping
+		[InlineData(1, "2025-03-16T09:00:01", "2025-03-16T09:00:02", AppointmentStatus.Completed, true)]  // Contained inside
+		[InlineData(1, "2025-03-16T08:45:00", "2025-03-16T09:00:01", AppointmentStatus.Completed, true)]  // Overlapping in front
+		[InlineData(1, "2025-03-16T10:00:00", "2025-03-16T10:30:00", AppointmentStatus.Completed, false)] // No overlap
+		[InlineData(1, "2025-03-16T09:00:00", "2025-03-16T09:30:00", AppointmentStatus.Completed, true)]  // Exact overlap
+		[InlineData(1, "2025-03-16T08:30:00", "2025-03-16T09:00:00", AppointmentStatus.Completed, false)] // Ends exactly when another starts
+		[InlineData(1, "2025-03-16T09:30:00", "2025-03-16T10:00:00", AppointmentStatus.Completed, false)] // Starts exactly when another ends
+		[InlineData(2, "2025-03-16T09:15:00", "2025-03-16T09:45:00", AppointmentStatus.Completed, false)] // Different doctor, overlapping
+		[InlineData(2, "2025-03-16T08:45:00", "2025-03-16T09:00:01", AppointmentStatus.Completed, false)] // Different doctor, overlapping in front
+		[InlineData(2, "2025-03-16T10:00:00", "2025-03-16T10:30:00", AppointmentStatus.Completed, false)] // Different doctor, no overlap
+		[InlineData(2, "2025-03-16T09:00:00", "2025-03-16T09:30:00", AppointmentStatus.Completed, false)] // Different doctor, exact overlap
+		[InlineData(2, "2025-03-16T08:30:00", "2025-03-16T09:00:00", AppointmentStatus.Completed, false)] // Different doctor, ends exactly when another starts
+		[InlineData(2, "2025-03-16T09:30:00", "2025-03-16T10:00:00", AppointmentStatus.Completed, false)] // Different doctor, starts exactly when another ends
 		public async Task HasConflict_ShouldReturnExpectedResult(
-			int newDoctorId, string newStartTime, string newEndTime, bool expectedResult)
+			int newDoctorId, string newStartTime, string newEndTime, AppointmentStatus status, bool expectedResult)
 		{
 			// Arrange
 			var options = GetInMemoryDbContextOptions();
@@ -56,7 +88,7 @@ namespace AppointmentScheduler.Tests
 					DoctorID = newDoctorId,
 					StartDate = DateTime.Parse(newStartTime),
 					EndDate = DateTime.Parse(newEndTime),
-					Status = AppointmentStatus.Scheduled
+					Status = status
 				};
 
 				// Act
@@ -68,3 +100,8 @@ namespace AppointmentScheduler.Tests
 		}
 	}
 }
+
+
+
+
+
